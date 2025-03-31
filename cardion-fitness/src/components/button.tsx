b14b5bf8735da8e11e8fba7cc6e1a3d35ddd51cd
americorodrigues25 <americo.200422@gmail.com>
2025-03-31 13:34:18 -0300

    first commit

---
+++ cardion-fitness/src/components/button.tsx
@@ -0,0 +1,16 @@
+// ButtonStyles.js
+import styled from 'styled-components/native';
+
+export const Button = styled.TouchableOpacity`
+  background-color: #00bfff;
+  padding: 10px 20px;
+  border-radius: 8px;
+  margin-top: 20px;
+`;
+
+export const ButtonText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+`;
