d48841786e716e82923492604ea8074fc3a419c5
americorodrigues25 <americo.200422@gmail.com>
2025-04-04 19:20:35 -0300

    Ajuste no background de loading

--- cardion-fitness/src/components/loadingBackgroundImage.jsx
+++ cardion-fitness/src/components/loadingBackgroundImage.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
 import { ImageBackground, View, ActivityIndicator } from 'react-native';
+import { useState } from 'react';
 
-export default function BackgroundWrapper({ children, source }) {
+export default function BackgroundImage({ children, source }) {
     const [imageLoaded, setImageLoaded] = useState(false);
 
     return (
         <View className="flex-1">
             <ImageBackground
                 source={source}
-                className="flex-1 justify-center items-center w-full h-full"
+                className="flex-1 justify-center items-center w-full h-full bg-colorInputs"
                 onLoadEnd={() => setImageLoaded(true)}
             >
                 {!imageLoaded && (
-                    <ActivityIndicator size="large" color="#6943FF" style={{ position: 'absolute' }} />
+                    <ActivityIndicator
+                        size="large"
+                        color="#6943FF"
+                        style={{ position: 'absolute' }}
+                    />
                 )}
-
                 {imageLoaded && children}
             </ImageBackground>
         </View>
