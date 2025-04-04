ab1696c3a79d61426e34d91341861e53a0c5a1cf
americorodrigues25 <americo.200422@gmail.com>
2025-04-04 19:22:29 -0300

    Melhorias na tela de splash

--- cardion-fitness/src/screens/shared/splash.jsx
+++ cardion-fitness/src/screens/shared/splash.jsx
@@ -1,44 +1,42 @@
-import { useEffect, useState } from 'react';
 import { Image, ActivityIndicator, SafeAreaView } from "react-native";
-import BackgroundWrapper from '~/components/loadingBackgroundImage'
+import { useEffect, useState } from 'react';
+
+import BackgroundImage from '~/components/loadingBackgroundImage';
 
 export default function Splash({ navigation }) {
-    const [showSplash] = useState(true);
-    const [showLoading, setShowLoading] = useState(false);
+    const [loadingIsVisible, setLoadingIsVisible] = useState(false);
 
     useEffect(() => {
         setTimeout(() => {
-            setShowLoading(true);
+            setLoadingIsVisible(true);
         }, 2500);
 
         setTimeout(() => {
-            setShowLoading(false);
+            setLoadingIsVisible(false);
             navigation.replace('userType');
         }, 5000);
     }, []);
 
     return (
-        <BackgroundWrapper
-            source={require('../../assets/img/imagemFundo1.png')}
+        <BackgroundImage
+            source={require('~/assets/img/backgroundImage/imagemFundo1.png')}
             style={{ resizeMode: 'contain' }}
         >
             <SafeAreaView className='flex-1 justify-center items-center'>
-                {showSplash && (
-                    <Image
-                        source={require('../../assets/img/Logo2.png')}
-                        className='w-8/12 absolute'
-                        style={{ resizeMode: 'contain' }}
-                    />
-                )}
-                {showLoading && (
+                <Image
+                    source={require('~/assets/img/logo/Logo2.png')}
+                    className='w-8/12 absolute'
+                    style={{ resizeMode: 'contain' }}
+                />
+                {loadingIsVisible && (
                     <ActivityIndicator
                         size="large"
                         color="#ffffff"
-                        style={{ position: 'absolute', bottom: '350' }}
+                        className="absolute bottom-[350px]"
                     />
                 )}
             </SafeAreaView>
-        </BackgroundWrapper>
+        </BackgroundImage>
 
     );
 }
