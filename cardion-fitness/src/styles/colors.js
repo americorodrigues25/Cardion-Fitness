6278a47f4765b798eb8e42afffd6e56a9660cbaf
americorodrigues25 <americo.200422@gmail.com>
2025-04-04 19:22:30 -0300

    Atualizando paleta de cores

--- cardion-fitness/src/styles/colors.js
+++ cardion-fitness/src/styles/colors.js
@@ -3,7 +3,6 @@ const colors = {
   colorDark200: '#18181B',
   colorDark300: '#09090B',
   colorDark400: '#000000',
-  colorLight100: '#F4F4F5',
   colorLight200: '#E4E4E7',
   colorLight300: '#D4D4D8',
   colorInputs: '#171c26',
