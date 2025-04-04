8787f529f46220deb4c3e09f7cd4fc345c67481b
americorodrigues25 <americo.200422@gmail.com>
2025-04-04 19:24:35 -0300

    Adicionando novos componentes, criação de uma subpasta 'logo' dentro de 'img' e troca do nome de uma função

---
+++ cardion-fitness/src/components/input.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/native";
+
+export const Input = styled.TextInput`
+    width: 100%;
+    padding: 25px 40px;
+    background-color: #171c26;
+    outline: none;
+    border: 1.5px solid #27272A;
+    border-radius: 16px;
+    margin-bottom: 8px;
+    font-size: 16px;
+    color: #D4D4D8;
+`;
