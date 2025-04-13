492361218a192c80f4c54a8f9231558645554510
americorodrigues25 <americo.200422@gmail.com>
2025-04-13 17:13:26 -0300

    refactor: atualiza estrutura da navegação principal

--- cardion-fitness/src/navigations/mainNavigation.jsx
+++ cardion-fitness/src/navigations/mainNavigation.jsx
@@ -1,19 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import AuthStack from './authStack';
-import AlunoStack from './alunoStack';
-import PersonalStack from './personalStack';
+import AlunoDrawer from './alunoDrawer';
+import PersonalDrawer from './personalDrawer';
+import AlunoNavigation from './alunoStack';
+import PersonalNavigation from './personalStack';
 
 const AppNavigation = () => {
-  // Podemos fazer desse jeito o controle de navegação e tipo de usuario né
-  const isLogged = false;
-  const userType = ''; 
+
+  const [isLogged, setIsLogged] = useState(null);
+  const [userType, setUserType] = useState(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const logged = await AsyncStorage.getItem('userLoggedIn');
+      const role = await AsyncStorage.getItem('role');
+
+
+      setIsLogged(logged === 'true');
+      setUserType(role);
+      setloading(false);
+    };
+
+    checkLogin();
+  }, []);
+
+  if (loading || isLogged  === null ) return null;
 
 
   return (
     <NavigationContainer>
       {!isLogged && <AuthStack />}
-      {isLogged && userType === 'aluno' && <AlunoStack />}
-      {isLogged && userType === 'personal' && <PersonalStack />}
+      {isLogged && userType === 'aluno' && <AlunoNavigation />}
+      {isLogged && userType === 'personal' && <PersonalNavigation />}
     </NavigationContainer>
   );
 };
