669add70cf0e0766adf22fea201375bc5c1c7de7
americorodrigues25 <americo.200422@gmail.com>
2025-04-13 17:11:52 -0300

    Ajuste de rota

--- cardion-fitness/src/navigations/alunoStack.jsx
+++ cardion-fitness/src/navigations/alunoStack.jsx
@@ -1,20 +1,16 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeAluno from '~/screens/aluno/home';
+import AlunoDrawer from './alunoDrawer';
 
 const Stack = createStackNavigator();
 
 const AlunoNavigation = () => {
     return (
-
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen
                 name="homeAluno"
-                component={HomeAluno}
+                component={AlunoDrawer}
             />
         </Stack.Navigator>
-
-
-
     );
 };
 
