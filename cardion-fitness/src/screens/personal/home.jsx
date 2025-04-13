4464d0fad99fde9f67f39cd1834be7c13eb2fa9b
americorodrigues25 <americo.200422@gmail.com>
2025-04-13 17:16:40 -0300

    feat: testes na navegação

--- cardion-fitness/src/screens/personal/home.jsx
+++ cardion-fitness/src/screens/personal/home.jsx
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { getAuth, signOut } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Home () {
+export default function Home() {
+
+    const handleLogout = async () => {
+        const auth = getAuth();
+        try {
+            await signOut(auth);
+            await AsyncStorage.removeItem('userLoggedIn');
+            navigation.replace('userType');
+        } catch (error) {
+            Alert.alert('Erro ao sair:', error)
+        }
+    };
 
     return (
         <View>
             <Text>Seja bem vindo(a), </Text>
+            <Text>Tela home personal </Text>
+
+            {/* Botão só pra fazer testes */}
+            <TouchableOpacity
+                onPress={handleLogout}
+                className='mt-5 bg-red-500 px-4 py-2 rounded'
+            >
+                <Text className='text-white font-bold'>Sair</Text>
+            </TouchableOpacity>
         </View>
     );
 }
