752fc9f3486b25502899f939e846c78e297e3222
americorodrigues25 <americo.200422@gmail.com>
2025-04-17 16:51:03 -0300

    feat(drawer): adiiona o drawerCustom

--- cardion-fitness/src/navigations/alunoDrawer.jsx
+++ cardion-fitness/src/navigations/alunoDrawer.jsx
@@ -1,13 +1,16 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import AlunoTabs from './alunoTabs';
 import Perfil from '~/screens/aluno/perfil';
+import CustomDrawerContent from './customDrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function AlunoDrawer() {
   return (
     <Drawer.Navigator
-      screenOptions={{ headerShown: true }}>
+      screenOptions={{ headerShown: true }}
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
       <Drawer.Screen name="Inicio" component={AlunoTabs} />
       <Drawer.Screen name="Meu Perfil" component={Perfil} />
     </Drawer.Navigator>
