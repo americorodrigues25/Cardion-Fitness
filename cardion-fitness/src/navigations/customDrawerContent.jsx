087c335b722935c3690b2ebb01ac78a3f789ff04
americorodrigues25 <americo.200422@gmail.com>
2025-04-17 16:52:03 -0300

    feat(drawer): cria conteúdo personalizado com imagem de perfil

---
+++ cardion-fitness/src/navigations/customDrawerContent.jsx
@@ -0,0 +1,61 @@
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { useEffect, useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { useGet } from '~/hook/crud/useGet';
+
+export default function CustomDrawerContent(props) {
+
+    const [nome, setNome] = useState();
+    const { getById } = useGet()
+
+        const trazerNome = async () => {
+            const user = await getById()
+            setNome(user.nome)
+            Alert.alert(user.nome)
+        }
+    
+        useEffect(() => {
+            const fetchNome = async () => {
+                await trazerNome();
+            };
+            fetchNome();
+        }, [])
+
+    const handleEditImage = () => {
+        Alert.alert(
+            'Alterar Foto de Perfil',
+            'Escolha uma opção',
+            [
+                { text: 'Tirar foto' },
+                { text: 'Escolher da galeria' },
+                { text: 'Remover foto' },
+                { text: 'Cancelar', style: 'destructive' },
+            ],
+            { cancelable: true }
+        );
+    };
+    return (
+        <DrawerContentScrollView {...props}>
+            <View style={{ alignItems: 'center', paddingVertical: 20 }}>
+                <View style={{ position: 'relative' }}>
+                    <Image
+                        source={require('~/assets/img/imgProfileDefault.png')}
+                        className='w-48 h-48 rounded-full'
+                    />
+                    <TouchableOpacity
+                        onPress={handleEditImage}
+                        className='absolute bottom-8 right-7 bg-colorLight200 rounded-full p-2'
+                    >
+                        <Ionicons name="camera" size={25} color="#000" />
+                    </TouchableOpacity>
+                </View>
+
+                <Text className='text-xl font-bold'>{nome}</Text>
+            </View>
+
+            <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+    );
+}
