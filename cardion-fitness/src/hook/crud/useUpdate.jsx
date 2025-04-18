fce3a72ea6a073816307f877b227a38b778e549b
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-04-18 18:11:05 -0300

    feat: termina implementação de hook para atualizar

--- cardion-fitness/src/hook/crud/useUpdate.jsx
+++ cardion-fitness/src/hook/crud/useUpdate.jsx
@@ -12,16 +12,23 @@ export const useUpdate = () =>
         // aqui o data sera um objeto contendo ou nao as propriedades
         // exemplo const user = {nome:"americo",telefone:""}
         // imagino que assim facilite pois só precisaremos passar um parametro
-        // porem devemos sempre olhar aqui pra ver se a propriedade esta sendo usada
+        // um detalhe muito importante é de passar todas as informacoes
+        // caso uma propriedade não seja passada a entidade sera atualizada com o valor null
+
         const updateDadosBasicos = async (data) =>{
             const role = await AsyncStorage.getItem("role")
             const uid = await AsyncStorage.getItem('uid')
-
+                
             // TODO: terminar de implementar
             await updateDoc(doc(db, role, uid), {
                 altura:data.altura,
                 dataNac:data.dataNasc,
-
+                sexo: data.sexo,
+                peso: data.peso,
+                altura: data.altura,
+                objetivo: data.objetivo,
+                xp: data.xp,
+                nivel: data.nivel,
               });
     
             return true
