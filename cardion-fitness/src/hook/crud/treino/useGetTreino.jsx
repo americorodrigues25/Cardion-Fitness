5eb03fbdc55aa3810f20131a2d65c0de2516d2cc
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-04-26 02:35:53 -0300

    feat: adiciona hook para listar treino

---
+++ cardion-fitness/src/hook/crud/treino/useGetTreino.jsx
@@ -0,0 +1,65 @@
+import {
+    getAuth,
+    } from 'firebase/auth';
+    
+    import { doc, getDoc,getDocs ,collection, query, where } from 'firebase/firestore';
+    import { useState } from 'react';
+    
+    // conexão Firebase
+    import { db, auth } from '../../firebase/firebaseConfig';
+    
+    import AsyncStorage from '@react-native-async-storage/async-storage';
+    
+    export const useGetTreino = () =>
+    {
+       
+        // pegar um
+        const getTreino = async (idAluno,idPersonal) =>{
+    
+            const docId = `${idAluno}_${idPersonal}`
+
+            const docRef = doc(db,"treino" , docId);
+            const docSnap = await getDoc(docRef);
+    
+            const treino = docSnap.data()
+    
+            return treino
+        }
+    
+        // pegar todos
+        const getAllTreinosByIdAluno = async (idAluno) =>{
+
+            const treinosRef = collection(db, 'treino');
+            const q = query(treinosRef, where('idAluno', '==', idAluno));
+            const querySnapshot = await getDocs(q);
+        
+            const treinos = [];
+            querySnapshot.forEach((doc) => {
+              treinos.push({
+                id: doc.id,        
+                ...doc.data()       
+              });
+            });
+        
+            return treinos;
+        }
+
+        const getAllTreinosByIdPersonal = async (idPersonal) =>{
+
+            const treinosRef = collection(db, 'treino');
+            const q = query(treinosRef, where('idPersonal', '==', idPersonal));
+            const querySnapshot = await getDocs(q);
+        
+            const treinos = [];
+            querySnapshot.forEach((doc) => {
+              treinos.push({
+                id: doc.id,        
+                ...doc.data()      
+              });
+            });
+        
+            return treinos;
+        }
+    
+        return{getTreino,getAllTreinosByIdAluno,getAllTreinosByIdPersonal}
+    }
