c03aac68b2cc5f3cf9434480ff2e42c8152609a4
americorodrigues25 <americo.200422@gmail.com>
2025-04-30 20:28:50 -0300

    feat(home aluno):implementa 'sessões realizadas', depois colocar na tela correta com funções corretas

--- cardion-fitness/src/screens/aluno/home.jsx
+++ cardion-fitness/src/screens/aluno/home.jsx
@@ -2,7 +2,11 @@ import { View, Text, TouchableOpacity, Alert, SafeAreaView } from 'react-native'
 import { useFocusEffect } from '@react-navigation/native';
 import { BackHandler } from 'react-native'; //esse é evento do botão voltar em Android
 import { useEffect, useState, useCallback } from 'react';
+
+import ProgressBar from 'react-native-progress/Bar';
+
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import { getAuth, signOut } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -22,7 +26,19 @@ export default function Home({ navigation }) {
     const [nome, setNome] = useState();
     const { getById } = useGet()
     const { deleteAccount } = useDelete()
-    const [usuario,setUsuario] = useState()
+    const [usuario, setUsuario] = useState()
+
+    //sessões de treinos realizadas
+    const totalSessoes = 40;
+    const [sessoes, setSessoes] = useState(0);
+    const progresso = sessoes / totalSessoes;
+
+    const marcarSessao = () => {
+        if (sessoes < totalSessoes) {
+            setSessoes(prev => prev + 1);
+
+        }
+    };
 
     const trazerNome = async () => {
         const user = await getById()
@@ -100,6 +116,43 @@ export default function Home({ navigation }) {
                 <TouchableOpacity onPress={() => gerarPdfUsuario(usuario)}>
                     <Text>Gerar PDF</Text>
                 </TouchableOpacity>
+
+                {/* sessões de treinos realizadas */}
+                <View className='mx-10 p-5 rounded-xl bg-colorDark200 mt-10'>
+                    <View className="flex-row items-center mb-10">
+                        <View className='flex-row justify-center items-center'>
+                            <View className='bg-colorViolet p-3 rounded-full'>
+                                < FontAwesome5 name='dumbbell' size={20} color='#E4E4E7' />
+                            </View>
+                            <View className='ml-5'>
+                                <Text className='text-colorLight200 text-2xl'>Sessões realizadas</Text>
+                                <Text className="text-colorViolet text-3xl">
+                                    {sessoes}
+                                    <Text className="text-gray-500 text-xl"> / {totalSessoes}</Text>
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <ProgressBar
+                        progress={progresso}
+                        width={null}
+                        height={12}
+                        color="#6943FF"
+                        unfilledColor="#eee"
+                        borderWidth={0}
+                        borderRadius={5}
+                        animated={true}
+                        animationType="spring"
+                        animationDuration={500}
+                    />
+
+                    <View className='px-10'>
+                        <TouchableOpacity className="bg-colorViolet mt-6 py-3 rounded" onPress={marcarSessao}>
+                            <Text className="text-center text-white font-semibold">Marcar sessão concluída</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         </SafeAreaView>
     );
