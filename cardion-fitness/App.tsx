1383cb208b455b49125011cfcdde5953c69219be
americorodrigues25 <americo.200422@gmail.com>
2025-04-30 18:27:59 -0300

    feat:implementa StatusBar para alterar a cor da bar

--- cardion-fitness/App.tsx
+++ cardion-fitness/App.tsx
@@ -1,11 +1,14 @@
+import { StatusBar } from 'react-native';
 import AppNavigation from '~/navigations/mainNavigation';
 import Toast from 'react-native-toast-message';
 
 import './global.css';
 
 export default function App() {
+
   return (
     <>
+      <StatusBar barStyle="light-content" />
       <AppNavigation />
       <Toast />
     </>
