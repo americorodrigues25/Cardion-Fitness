129a3d85148b420723e524d4efb5e83f12eb29db
americorodrigues25 <americo.200422@gmail.com>
2025-05-14 21:55:41 -0300

    feat: cria arquivos de avaliações fisicas

---
+++ cardion-fitness/src/screens/personal/avaliacoes.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform, Image, ActivityIndicator } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Entypo from 'react-native-vector-icons/Entypo'
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function AvaliacaoScreen() {
+    const navigation = useNavigation();
+
+    const handleAvaliacao = (avaliacao) => {
+        navigation.navigate('DetalhesAvaliacao', { avaliacao });
+    };
+
+    return (
+        <SafeAreaView edges={["top"]} className="flex-1 bg-colorBackground">
+            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+                <ScrollView
+                    bounces={false}
+                    overScrollMode="never"
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ flexGrow: 1 }}
+                >
+                    <View className="pt-5 px-5 pb-5">
+                        <TouchableOpacity onPress={() => navigation.goBack()} className="flex-row">
+                            <Image source={require('~/assets/img/btnVoltar.png')} className="w-4 h-5" />
+                            <Text className="ml-2 text-colorLight200">Avaliação</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View className='px-5 py-5'>
+                        <Text className="text-center text-gray-500">Nenhuma avaliação registrada.</Text>
+
+                        {/* cards q será exibido a avaliação criada (Avaliação - Data: DD/MM/AAAA) - ao clicar, abrir a tela detalhes da avaliação */}
+                        <TouchableOpacity
+                            onPress={() => handleAvaliacao()}
+                            className="bg-gray-100 p-4 rounded-xl mb-3"
+                        >
+                            <Text>Avaliações</Text>
+                        </TouchableOpacity>
+
+
+                        <TouchableOpacity
+                            className="flex-row items-center bg-colorViolet rounded-full py-3 justify-center"
+                            onPress={() => navigation.navigate('CriarAvaliacao')}
+                        >
+                            <View className="flex-row items-center gap-x-1">
+                                <Entypo name="plus" size={20} color="#E4E4E7" />
+                                <Text className="text-colorLight200 text-base font-semibold">Criar novo treino</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </SafeAreaView >
+    );
+}
---
+++ cardion-fitness/src/screens/personal/detalhesAvaliacao.jsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+
+export default function DetalhesAvaliacao() {
+
+  return (
+    <View></View>
+  );
+}
