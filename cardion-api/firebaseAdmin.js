9c7c3967ea5917b987d81c6479cabbd3d1a40091
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-05-24 03:04:34 -0300

    chore: configura chamada de secret

--- cardion-api/firebaseAdmin.js
+++ cardion-api/firebaseAdmin.js
@@ -1,6 +1,7 @@
 // firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./cardion-95801-firebase-adminsdk-fbsvc-0ef1a4ce30.json');
+const serviceAccount = require('./contaadmin.json');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
