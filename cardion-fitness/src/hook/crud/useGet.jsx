2b62604f40cf348945eee5c1287fa90cf82ba89f
americorodrigues25 <americo.200422@gmail.com>
2025-05-25 18:58:34 -0300

    feat: implementa nova lógica para trarzer pontuação somente dos desafios

--- cardion-fitness/src/hook/crud/useGet.jsx
+++ cardion-fitness/src/hook/crud/useGet.jsx
@@ -2,7 +2,7 @@ import {
   getAuth,
 } from 'firebase/auth';
 
-import { doc, getDoc, getDocs, collection, query, where,orderBy } from 'firebase/firestore';
+import { doc, getDoc, getDocs, collection, query, where, orderBy } from 'firebase/firestore';
 import { useState } from 'react';
 
 // conexão Firebase
@@ -42,7 +42,7 @@ export const useGet = () => {
     console.log("Iniciando busca de alunos vinculados...");
 
     const role = await AsyncStorage.getItem("role")
-    const uid = await AsyncStorage.getItem("uid") 
+    const uid = await AsyncStorage.getItem("uid")
 
     console.log("role:", role, "uid:", uid);
 
@@ -102,7 +102,7 @@ export const useGet = () => {
   }
 
 
-  
+
   // Trazer dados do aluno pelo id dele, nesse caso chamado de detalhe do aluno
   const getAlunoById = async (idAluno) => {
 
@@ -115,7 +115,7 @@ export const useGet = () => {
   }
 
 
-    // Trazer dados do aluno pelo id dele, nesse caso chamado de detalhe do aluno
+  // Trazer dados do aluno pelo id dele, nesse caso chamado de detalhe do aluno
   const getPersonalById = async (idPersonal) => {
 
     const docRef = doc(db, 'personal', idPersonal);
@@ -151,14 +151,26 @@ export const useGet = () => {
 
 
   const buscarAlunosOrdenadosPorPontos = async () => {
-  const q = query(collection(db, 'aluno'), orderBy('pontos', 'desc'));
-  const querySnapshot = await getDocs(q);
+    const q = query(collection(db, 'aluno'), orderBy('pontos', 'desc'));
+    const querySnapshot = await getDocs(q);
 
-  return querySnapshot.docs.map(doc => ({
-    id: doc.id,
-    nome: doc.data().nome,
-    pontos: doc.data().pontos
-  }));
+    return querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      nome: doc.data().nome,
+      pontos: doc.data().pontos
+    }));
   };
-  return { getById, getAll, getAllAlunosByPersonal, getAlunoByEmail,getAlunoById, getPersonalById,getPersonalDoAluno,buscarAlunosOrdenadosPorPontos}
+
+  const buscarAlunosOrdenadosPorPontosDesafios = async () => {
+    const q = query(collection(db, 'aluno'), orderBy('pontosDesafios', 'desc'));
+    const querySnapshot = await getDocs(q);
+
+    return querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      nome: doc.data().nome,
+      pontosDesafios: doc.data().pontosDesafios || 0,
+    }));
+  };
+  
+  return { getById, getAll, getAllAlunosByPersonal, getAlunoByEmail, getAlunoById, getPersonalById, getPersonalDoAluno, buscarAlunosOrdenadosPorPontos, buscarAlunosOrdenadosPorPontosDesafios }
 }
