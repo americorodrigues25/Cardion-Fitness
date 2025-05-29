75ba11860094764086a51328028c8d2f5fd2488d
MarcosMartinsDeLima <0timetraveler@gmail.com>
2025-05-28 23:19:29 -0300

    feat: adiciona função para se desvincular de personal

--- cardion-fitness/src/hook/crud/vincularAlunos/vincularAluno.jsx
+++ cardion-fitness/src/hook/crud/vincularAlunos/vincularAluno.jsx
@@ -80,5 +80,84 @@ export const useVinculo = () => {
     await Promise.all(promises);
   }
 
-  return { vincularAluno, desvincularAluno };
+  const seDesvincularDePersonal = async (idPersonal) =>{
+    const idAluno = await AsyncStorage.getItem('uid');
+    try{
+
+    
+    if (!idPersonal || !idAluno) {
+       Toast.show({
+                type: 'error',
+                text1: 'Aluno ou Personal não encontrado',
+            });
+    }
+
+    // vinculo
+    const vinculosRef = collection(db, 'vinculos');
+    const q = query(
+      vinculosRef,
+      where('idPersonal', '==', idPersonal),
+      where('idAluno', '==', idAluno)
+    );
+
+    const snapshot = await getDocs(q);
+
+
+    const promises = snapshot.docs.map((docSnap) =>
+      deleteDoc(doc(db, 'vinculos', docSnap.id))
+    );
+
+    await Promise.all(promises);
+
+    // treino
+    const treinosRef = collection(db, 'treino');
+    const q2= query(
+      treinosRef,
+      where('idPersonal', '==', idPersonal),
+      where('idAluno', '==', idAluno)
+    );
+
+    const snapshot2 = await getDocs(q2);
+
+
+    const promises2 = snapshot2.docs.map((docSnap) =>
+      deleteDoc(doc(db, 'treino', docSnap.id))
+    );
+
+    await Promise.all(promises2);
+
+
+    // avaliacao
+    const avaliacaoRef = collection(db, 'avaliacaoFisica');
+    const q3= query(
+      avaliacaoRef,
+      where('idPersonal', '==', idPersonal),
+      where('idAluno', '==', idAluno)
+    );
+
+    const snapshot3 = await getDocs(q3);
+
+
+    const promises3 = snapshot3.docs.map((docSnap) =>
+      deleteDoc(doc(db, 'avaliacaoFisica', docSnap.id))
+    );
+
+    await Promise.all(promises3);
+
+     Toast.show({
+      type: 'success',
+      text1: 'Parabéns ✅',
+      text2: 'Personal desvinculado!',
+    });
+
+  }catch(e){
+
+     Toast.show({
+                type: 'error',
+                text1: 'Aluno ou Personal não encontrado',
+            });
+  }
+}
+
+  return { vincularAluno, desvincularAluno,seDesvincularDePersonal };
 };
