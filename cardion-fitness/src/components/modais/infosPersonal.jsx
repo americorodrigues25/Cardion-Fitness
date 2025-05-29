ce03d241365c74724e7310fd54451101c93f03f4
americorodrigues25 <americo.200422@gmail.com>
2025-05-29 01:01:29 -0300

    refactor: Modifica informações que serão trazidas para tela, referente ao personal

--- cardion-fitness/src/components/modais/infosPersonal.jsx
+++ cardion-fitness/src/components/modais/infosPersonal.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Modal, View, Text, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Entypo, Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 
-export default function InfosPersonal({ visible, onClose, personal, openWhatsApp, sendEmail }) {
+export default function InfosPersonal({ visible, onClose, personal, anotacao }) {
     return (
         <Modal
             visible={visible}
@@ -13,48 +13,17 @@ export default function InfosPersonal({ visible, onClose, personal, openWhatsApp
         >
             <View className="flex-1 justify-center items-center bg-black/80 px-5">
                 <View className="bg-colorDark100 rounded-2xl p-6 w-full max-h-[80%]">
-                    <Text className="text-lg font-bold mb-5 text-colorLight200">Informações do Personal</Text>
 
-                    <View className="gap-y-1">
-                        <View className="flex-row items-center gap-x-2">
-                            <Ionicons name="person" size={20} color="#6943FF" />
-                            <Text className="text-colorLight200 font-bold">Nome: {personal?.nome || "Não informado"}</Text>
+                    <View className="mt-4">
+                        <View className='flex-row items-center gap-x-2 mb-2'>
+                            <Entypo name='message' size={25} color='#6943FF' />
+                            <Text className="text-lg font-bold text-colorLight200">Feedback do seu personal:</Text>
                         </View>
-
-                        <View className="flex-row items-center gap-x-2">
-                            <FontAwesome name="phone" size={20} color="#6943FF" />
-                            <Text className="text-colorLight200 font-bold">Telefone: {personal?.telefone || "Não informado"}</Text>
-                        </View>
-
-                        <View className="flex-row items-center gap-x-2">
-                            <FontAwesome name="envelope" size={20} color="#6943FF" />
-                            <Text className="text-colorLight200 font-bold">Email: {personal?.email || "Não informado"}</Text>
-                        </View>
+                        <Text className="text-colorLight300">{anotacao || 'Nenhum feedback no momento'}</Text>
                     </View>
 
-                    {(personal?.telefone || personal?.email) && (
-                        <View className="mt-6 gap-y-2 mx-10">
-                            {personal?.telefone && (
-                                <TouchableOpacity
-                                    onPress={() => openWhatsApp(personal.telefone)}
-                                    className="border border-green-500 rounded-md py-2 px-4"
-                                >
-                                    <Text className="text-green-500 font-semibold text-center">Falar via WhatsApp</Text>
-                                </TouchableOpacity>
-                            )}
-                            {personal?.email && (
-                                <TouchableOpacity
-                                    onPress={() => sendEmail(personal.email)}
-                                    className="border border-colorViolet rounded-md py-2 px-4"
-                                >
-                                    <Text className="text-colorViolet font-semibold text-center">Enviar Email</Text>
-                                </TouchableOpacity>
-                            )}
-                        </View>
-                    )}
-
-                    <TouchableOpacity onPress={onClose} className="mt-5">
-                        <Text className="text-colorViolet text-right font-bold">Fechar</Text>
+                    <TouchableOpacity onPress={onClose} className="mt-5 bg-colorViolet px-10 py-2 rounded-full mx-14">
+                        <Text className="text-colorLight200 font-bold text-center">Fechar</Text>
                     </TouchableOpacity>
                 </View>
             </View>
